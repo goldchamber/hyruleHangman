f14435ce5732238106db8e914f8f6738a30f0510
goldchamber <49383727+goldchamber@users.noreply.github.com>
2019-05-05 09:29:38 -0700

    refactored

--- main.cpp
+++ main.cpp
@@ -8,80 +8,97 @@
 
 using namespace std;
 
+string randomizer(vector<string> v);
+int playAgain();
+char enterInput();
+
+
 int main()
 {
-	const int MAX_WRONG = 8; 
+	string n[] = { "LINK", "ZELDA", "NAVI", "EPONA", "GANON",
+				   "SHEIK", "CIA", "MIDNA", "IMPA", "ZANT",
+				   "RUTO", "NAYRU" };
 	vector<string> words;
-	words.push_back("ZELDA");
-	words.push_back("LINK");
-	words.push_back("GANON");
-	words.push_back("SHEIK");
-	words.push_back("LINKLE");
-	words.push_back("MIDNA");
-	words.push_back("EPONA");
-	words.push_back("NAVI");
-	words.push_back("MAJORA");
-	words.push_back("CIA");
+	words.insert(words.end(), begin(n), end(n));
 
-	srand(static_cast<unsigned int>(time(0)));
-	random_shuffle(words.begin(), words.end());
-	
-	const string THE_WORD = words[0];
-	int wrong = 0;
+	const int MAX_WRONG = 8;
+	int attempts = 0;
+	char guess;
+	string used;
+
+	const string THE_WORD = randomizer(words);
 	string soFar(THE_WORD.size(), '_');
-	string used = "";
 
-	cout << "WELCOME TO HYRULE HANGMAN!";
+	cout << "\n\nWelcome to Hyrule Hangman!\n\n";
 
-
-	//MAIN GAME LOOP
-	while ((wrong < MAX_WRONG) && (soFar != THE_WORD)) {
-		cout << "\n\nYou have " << (MAX_WRONG - wrong);
-		cout << " incorrect guesses left.\n";
-		cout << "\nYou've used the following letters:\n" << used << endl;
+	while (attempts < MAX_WRONG && soFar != THE_WORD) {
+		cout << "You have " << MAX_WRONG - attempts << " attempts remaining.\n";
 		cout << soFar;
+		guess = enterInput();
 
-		char guess;
-		cout << "\n\nEnter your guess: ";
-		cin >> guess;
-		guess = toupper(guess);
-		while (used.find(guess) != string::npos) {
-			cout << "\nYou've already guessed " << guess << endl;
-			cout << "Enter your guess: ";
-			cin >> guess;
-			guess = toupper(guess);
-		}
-		used += guess;
 
-		
 		while (!isalpha(guess)) {
-			cout << guess << " is not a valid choice.\n";
-			cout << "\n\nEnter your guess: ";
-			cin >> guess;
+			cout << guess << " is not a valid answer. Please enter a character.\n";
+			guess = enterInput();
+		}
+
+		while (used.find(guess) != string::npos) {
+			cout << "\nYou've already used " << guess << ". Guess again.\n";
+			guess = enterInput();
+		}
+
+		if (!isalpha(guess)) {
+			cout << "That is not a valid guess. Please enter a character.";
 		}
 
 		if (THE_WORD.find(guess) != string::npos) {
-			cout << "That's right! " << guess << " is in the word.\n";
+			cout << guess << " is in the word!\n";
 			for (int i = 0; i < THE_WORD.length(); i++) {
 				if (THE_WORD[i] == guess) {
 					soFar[i] = guess;
+					used += guess;
 				}
 			}
 		}
 		else {
-			cout << "Sorry, " << guess << " isn't in the word.\n";
-			++wrong;
+			cout << guess << " is not in the word. Try again.\n";
+			used += guess;
+			attempts++;
 		}
-	} 
-
-	if (wrong == MAX_WRONG) {
-		cout << "\nYou've been hanged!";
+	}
+	if (attempts == MAX_WRONG) {
+		cout << "\nGAME OVER\n";
+		cout << "RETURN OF GANON\n";
 	}
 	else {
-		cout << "\n You guessed it!";
+		cout << "\nYou guessed it!";
+		cout << "\nYou're the hero of Hyrule\n!";
 	}
+	cout << "\nThe word was " << THE_WORD << "!" << endl;
+	playAgain();
+}
 
-	cout << "\nThe word was " << THE_WORD << endl;
-
-	return 0;
+string randomizer(vector<string> v) {
+	srand(static_cast<unsigned int>(time(0)));
+	random_shuffle(v.begin(), v.end());
+	const string THE_WORD = v[0];
+	return THE_WORD;
+}
+int playAgain() {
+	char yesNo;
+	cout << "Would you like to play again? (y/n)\n";
+	cin >> yesNo;
+	if (yesNo == 'y') {
+		main();
+	}
+	else {
+		cout << "Goodbye!\n\n\n";
+		return 0;
+	}
+}
+char enterInput(){
+	char g;
+	cout << "\nEnter your guess: ";
+	cin >> g;
+	return toupper(g);
 }
